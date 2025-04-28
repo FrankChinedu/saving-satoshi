fd5876a1c54039ea1d0f0adabe66e22a58b6086c
Obi Chinedu Frank <frankieetchy@gmail.com>
2025-04-28 14:34:13 +0100

    msg to integer

--- utils/index.js
+++ utils/index.js
@@ -57,5 +57,21 @@ const hashedPublicKey = hashCompressed(
 );
 const publicKey = compressPublicKey(uncompressedKey);
 
+const msg =
+  '0100000001c997a5e56e104102fa209c6a852dd90660a20b2d9c352423edce25857fcd37040000000043410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3acffffffff0200ca9a3b00000000434104ae1a62fe09c5f51b13905f07f06b99a2f7159b2225f374cd378d71302fa28414e7aab37397f554a7df5f142c21c1b7303b8a0626f1baded5c72a704f7e6cd84cac00286bee0000000043410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3ac0000000001000000';
+
+const sha256 = (buffer) => {
+  return crypto.createHash('sha256').update(buffer).digest();
+};
+
+function msg_to_integer(msg) {
+  console.log({ msg });
+  const buffer = Buffer.from(msg, 'hex');
+  const sha256Hash = sha256(sha256(buffer));
+  return BigInt(`0x${sha256Hash.toString('hex')}`);
+  // Given a hex string to sign, convert that string to a Buffer of bytes,
+  // double-SHA256 the bytes and then return a BigInt() from the 32-byte digest.
+}
+
 exports.hashedPublicKey = hashedPublicKey;
 exports.publicKey = publicKey;
